b29b6ad406c46faee35bae0b346a0157582a1214
Radu Elisei <45122923+raduElisei@users.noreply.github.com>
2023-10-17 13:47:31 +0300

    Add project files.

---
+++ ArrayHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public static class ArrayHelper
+    {
+        static int min = int.MaxValue;
+        static int max = int.MinValue;
+
+        public static (int, int) ArrayMinMax(int[] array)
+        {
+            if (array.Length > 0)
+            {
+                if (array[0] > max)
+                    max = array[0];
+
+                if (array[0] < min)
+                    min = array[0];
+
+                array = array.Skip(1).ToArray();
+
+                ArrayMinMax(array);
+            }
+
+            return (min, max);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab5;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] myArray = { 1, 2, 3, 34, 5, 6, 7, 8, 9, -3, 11, 12, 13, 1 };
+
+        (int, int) results = ArrayHelper.ArrayMinMax(myArray);
+        Console.WriteLine(results);
+    }
+}
